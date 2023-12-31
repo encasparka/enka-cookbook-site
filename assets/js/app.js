685ac13dc3f8d21d9c623fdb46eaa1998c0f92bf
QaldeK <qaldek@gmx.com>
2023-12-31 16:40:58 +0100

    nop.. re-assets

---
+++ assets/js/app.js
@@ -0,0 +1,15 @@
+// app.js for concatenation of smaller libraryies
+// to reduce http requests of small files
+'use strict';
+
+import bootstrap from 'js/vendor/bootstrap.bundle.js';
+import alpinejs from 'js/vendor/alpine.min.js';
+
+// // Prefetch in-viewport links during idle time
+// import { listen } from 'quicklink/dist/quicklink.mjs';
+// listen();
+
+// // lazy sizes for image loading
+// import 'lazysizes';
+
+
